04290c3464e04b207b64a2a5e0a5e0b9d09f7c5c
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-01-13 22:35:52 -0800

    Add some styling to test it out

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,45 @@
 	import 'normalize.css';
 </script>
 
-<nav>
-	<a href="/">jk reviews</a>
-</nav>
+<header class="header">
+	<a href="/" class="home">JK Reviews</a>
+	<nav>
+		<a href="/books">Books</a>
+		<a href="/movies">Movies</a>
+		<a href="/anime">Anime</a>
+	</nav>
+</header>
 
-<slot />
+<slot class="main" />
+
+<footer>
+	<a href="https://juliankrzysiak.com">© 2024 Julian Krzysiak</a>
+	<a href="https://github.com/CastillejaCode/reviews">github </a>
+</footer>
+
+<style>
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100vh;
+	}
+
+	.main {
+		flex: 1;
+	}
+
+	header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+		padding: 1rem;
+	}
+
+	footer {
+		display: flex;
+		justify-content: space-between;
+		padding: 1rem;
+	}
+</style>
